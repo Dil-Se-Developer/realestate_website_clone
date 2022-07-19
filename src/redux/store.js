b227c49e7aadbf0aaf824f07e116f61910cdc23d
Shail_Office <shailshah.officework@gmail.com>
2022-07-19 11:27:18 +0530

    Redux store code changed --office

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,33 @@
-import { applyMiddleware, createStore, compose } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import reducers from "./reducers/index";
+import rootReducer from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
 
 const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store;
+
+
+
+// Old Code
+
+// import { applyMiddleware, createStore, compose } from "redux";
+// import thunk from "redux-thunk";
+// import reducers from "./reducers/index";
+
+// // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   reducers,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+// export default store;
